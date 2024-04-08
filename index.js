3e5956b4f922ff80576bfdff2d3bd6475d7a1317
680ashish <680ashish@gmail.com>
2024-04-09 02:48:04 +0600

    adding responsiveness which I have done just now

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const myCarouselElement = document.querySelector('#myCarousel')
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
+// console.log(temp);
 
+function counter(){
+  
+  let count=0;
+
+  let h3counter= document.querySelectorAll('.details-cards h3');
+
+  setInterval(() => {
+    if(count<69){
+      count++;
+      h3counter.forEach(h3 => {
+        h3.innerHTML = count;
+      });
+  }
+  else{
+      count=69;
+  }
+  }, 20);
+}
+counter();
